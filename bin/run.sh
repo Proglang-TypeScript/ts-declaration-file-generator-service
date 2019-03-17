ad490b63a0258eb2b7a056085254217bb87b80ca
Fernando Cristiani <fer5cristiani@gmail.com>
2019-03-17 14:58:47 +0100

    Add information to run command

--- bin/run.sh
+++ bin/run.sh
@@ -12,7 +12,10 @@ RUNTIME_INFO=$ROOT_PATH/tmp/output.json
 GET_RUNTIME_INFORMATION="$ROOT_PATH/cli/getRunTimeInformation.sh"
 GENERATE_DECLARATION_FILE="$ROOT_PATH/cli/generateDeclarationFile.sh"
 
+echo "Generating runtime information ..."
 $GET_RUNTIME_INFORMATION $JS_PROJECT $RUNTIME_INFO
+
+echo "Generating declaration file ..."
 $GENERATE_DECLARATION_FILE $RUNTIME_INFO $MODULE_NAME $RESULTS_DIR
 
 rm -f RUNTIME_INFO
