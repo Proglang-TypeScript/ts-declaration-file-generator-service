8fe9ebeb28957bd6a2b898794a41e402650dfda9
Fernando Cristiani <fer5cristiani@gmail.com>
2020-09-24 16:23:51 +0200

    Bump version from v1.1 to v1.2

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 INSTALLERS=$SCRIPT_PATH/install
 
-VERSION=v1.1
+MASTER_MIND_VERSION=v1.1
+DTS_GENERATE_VERSION=v1.2
 
-$INSTALLERS/master-mind/install.sh $SCRIPT_PATH $VERSION
-$INSTALLERS/ts-declaration-file-generator/install.sh $SCRIPT_PATH $VERSION
+$INSTALLERS/master-mind/install.sh $SCRIPT_PATH $MASTER_MIND_VERSION
+$INSTALLERS/ts-declaration-file-generator/install.sh $SCRIPT_PATH $DTS_GENERATE_VERSION
 
 rm -rf $SCRIPT_PATH/tmp
 mkdir $SCRIPT_PATH/tmp
